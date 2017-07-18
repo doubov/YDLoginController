435edbf65ede91f1fec9f6ffee4ec28899eaf6da
Yuri Doubov <yuri@vitalhub.com>
2017-07-18 09:52:40 -0400

    Better usage example

--- YDLoginController/ViewController.swift
+++ YDLoginController/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	
+	override func viewDidAppear(_ animated: Bool) {
+		
+		let loginController = YDLoginController.init(nibName: nil, bundle: nil)
+		
+		// Replace the default background
+		loginController.style.imgBackground = #imageLiteral(resourceName: "CustomBackground")
+		
+		// Replace the default logo
+		loginController.style.imgLogo = #imageLiteral(resourceName: "CustomLogo")
+		
+		// Set the user/password icon tint to orange
+		loginController.style.colorIconTint = UIColor.init(red: 234/255, green: 120/255, blue: 37/255, alpha: 1)
+		
+		self.present(loginController, animated: true, completion: nil)
+		
+		super.viewDidAppear(animated)
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
 
 }
 
